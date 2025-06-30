c6021199e508f706567c0ef3bf53ca09610e9b24
Christian Souza <christian.rodriguesdesouza76@gmail.com>
2025-06-30 14:08:35 -0300

    feat: tela inicial OK

---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-welcome',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  templateUrl: './welcome.component.html',
+  styleUrl: './welcome.component.scss'
+})
+
+export class WelcomeComponent {
+  userName: string = '';
+
+  constructor(private router: Router) { }
+
+  enterApp(): void {
+    if (this.userName.trim()) {
+      localStorage.setItem('userName', this.userName.trim());
+      this.router.navigate(['/clients']);
+    } else {
+      alert('Por favor, digite seu nome para continuar!');
+    }
+  }
+}
